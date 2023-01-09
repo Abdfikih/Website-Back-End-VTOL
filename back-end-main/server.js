1bfd108041e4bcd18621aa2be8de7950415ccb69
Abdul <abdul.fikih@ui.ac.id>
2023-01-09 16:47:54 +0700

    add strict query

--- back-end-main/server.js
+++ back-end-main/server.js
@@ -6,6 +6,7 @@ dotenv.config({
   path: "./config.env",
 });
 
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));

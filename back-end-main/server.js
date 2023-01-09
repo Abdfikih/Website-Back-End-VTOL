bbe68acbd2f33e90a086526ffa6b127c6e09b35e
Abdul <abdul.fikih@ui.ac.id>
2023-01-09 12:51:47 +0700

    edit env

--- back-end-main/server.js
+++ back-end-main/server.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
-const port = process.env.PORT || 5000;
+const port = process.env.NODE_ENV || 5000;
 app.listen(port, () =>
   console.log(`App running on port ${port}... in ${process.env.NODE_ENV} mode`)
 );

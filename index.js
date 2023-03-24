bc880324ec58180c9c509544951640aa4006466c
Abdul <abdul.fikih@ui.ac.id>
2023-03-24 23:08:50 +0700

    tesetr apiaa

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use(function (req, res, next) {
